9713b6e0c27a070162abc87bfd73d46776036954
Bloodyaugust <greysonrichey@gmail.com>
2021-01-31 14:41:28 -0700

    Start integrating ServerHook into main

--- scripts/controllers/player_controller.gd
+++ scripts/controllers/player_controller.gd
@@ -3,18 +3,19 @@ extends Node
 export var _fleet_actor: PackedScene
 
 onready var _fleets_container: Node2D = $"../Fleets"
+onready var _server_hook: Node = $"../ServerHook"
 
 func _on_state_changed(state_key: String, substate):
   match state_key:
-    "game":
-      match substate:
-        GameConstants.GAME_STARTING:
-          GDUtil.queue_free_children(_fleets_container)
+    "systems":
+      if Store.state.game == GameConstants.GAME_OVER:
+        _server_hook._request_galaxy()
+    "uid":
+      GDUtil.queue_free_children(_fleets_container)
 
-          # TODO: Move this code to after we have registered, which should trigger a change to GameConstants.GAME_IN_PROGRESS
-          var _new_fleet: Node2D = _fleet_actor.instance()
-          _fleets_container.add_child(_new_fleet)
-          Store.set_state("selection", _new_fleet)
+      var _new_fleet: Node2D = _fleet_actor.instance()
+      _fleets_container.add_child(_new_fleet)
+      Store.set_state("selection", _new_fleet)
 
 func _ready():
   Store.connect("state_changed", self, "_on_state_changed")
