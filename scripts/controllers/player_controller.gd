299ed1902aa22c5f548e4304db353bd13a1026c6
Bloodyaugust <greysonrichey@gmail.com>
2021-01-31 12:56:04 -0700

    Auto-select player fleet at game starting

--- scripts/controllers/player_controller.gd
+++ scripts/controllers/player_controller.gd
@@ -14,6 +14,7 @@ func _on_state_changed(state_key: String, substate):
           # TODO: Move this code to after we have registered, which should trigger a change to GameConstants.GAME_IN_PROGRESS
           var _new_fleet: Node2D = _fleet_actor.instance()
           _fleets_container.add_child(_new_fleet)
+          Store.set_state("selection", _new_fleet)
 
 func _ready():
   Store.connect("state_changed", self, "_on_state_changed")
