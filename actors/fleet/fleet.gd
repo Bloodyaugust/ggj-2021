c9b976ab7903d4be563555ca9c8de9ae5cf9a8bf
Bloodyaugust <greysonrichey@gmail.com>
2021-02-02 02:46:59 -0700

    Rework fuel consumption to be predictable
    - Fix fuel reachability calculation
    - Implement new method to find fuel consumption to travel to system

--- actors/fleet/fleet.gd
+++ actors/fleet/fleet.gd
@@ -5,7 +5,10 @@ enum fleet_states {
   MOVING
 }
 
+# Fuel consumed per distance unit
+export var fuel_consumption: float
 export var move_range: float
+# Distance unit moved per second
 export var speed: float
 
 onready var _area2d: Area2D = $"./Area2D"
@@ -14,9 +17,11 @@ onready var _sprite: Sprite = $"./Sprite"
 
 var _current_scale: float
 var _current_state: int
-var _distance: float
 var _selected: bool
 var _target: Node2D
+
+func get_fuel_consumption_to_target(star_system: Node2D) -> float:
+  return fuel_consumption * global_position.distance_to(star_system.global_position)
 
 func is_in_range(star_system: Node2D) -> bool:
   return star_system.global_position.distance_to(global_position) <= move_range
@@ -35,9 +40,9 @@ func _on_store_state_changed(state_key, substate):
     "target":
       if _selected && _current_state == fleet_states.IDLE && substate != null && global_position.distance_to(substate.global_position) <= move_range:
         _target = substate
-        _distance = global_position.distance_to(_target.global_position)
-        var _req = stepify(0.005*_distance, 1)
-        if _req <= Clientstore.get_state("fuel"):
+
+        var _required_fuel = get_fuel_consumption_to_target(_target)
+        if _required_fuel <= Clientstore.get_state("fuel"):
           _current_state = fleet_states.MOVING
         else:
           print("whoops, not enough fuel!")
@@ -58,14 +63,17 @@ func _process(delta):
     fleet_states.MOVING:
       if (Clientstore.get_state("fuel_tick")!=0):
         Clientstore.set_state("fuel_tick",0)
-      if _target.global_position.distance_squared_to(global_position) < (speed):
+      if _target.global_position.distance_to(global_position) < speed * delta:
         _current_state = fleet_states.IDLE
+        global_position = _target.global_position
         Store.emit_signal("fleet_arrived", self, _target)
         continue
 
       look_at(_target.global_position)
       translate((_target.global_position - global_position).normalized() * speed * delta)
-      Clientstore.set_state("fuel", Clientstore.get_state("fuel") - stepify(0.005*(_distance - global_position.distance_to(_target.global_position)), 1))
+
+      var _fuel_consumed = (speed * delta) * fuel_consumption
+      Clientstore.set_state("fuel", Clientstore.get_state("fuel") - _fuel_consumed)
 
   update()
 
